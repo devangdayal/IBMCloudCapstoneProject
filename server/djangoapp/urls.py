99c017e5506958f49c3ae4acd57fa3202d35a9e4
devangdayal <devangdayal@gmail.com>
2023-12-20 09:03:51 -0500

    Stage 1 cleared

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,28 +1,17 @@
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # route is a string contains a URL pattern
-    # view refers to the view function
-    # name the URL
-
-    # path for about view
-
-    # path for contact us view
-
-    # path for registration
-
-    # path for login
-
-    # path for logout
-
-    path(route='', view=views.get_dealerships, name='index'),
-
-    # path for dealer reviews view
-
-    # path for add a review view
-
+    #path('', views.index, name='index'),  # You might want to uncomment and include this line if 'index' is intended
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('register/', views.registration_request, name='register'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
+    path('get-dealerships/', views.get_dealerships, name='get_dealerships'),
+    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
+    path('dealer/<int:dealer_id>/add-review/', views.add_review, name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
